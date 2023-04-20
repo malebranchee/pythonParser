2d1cf8b533d5d87f782ccec2b4da83fc1afff88c
malebranchee <paho3998@mail.ru>
2023-04-20 15:36:57 +0600

    Update 1.0

--- mainfile.py
+++ mainfile.py
@@ -9,10 +9,6 @@ from openpyxl.reader.excel import load_workbook
 
 
 def parse():
-    
-    doc = load_workbook(filename='goo.xlsx')
-    ws = doc.active #Создание книги Excel и рабочего листа
-
 
     url = 'https://www.chitai-gorod.ru/' #Запрос на сайт
     page = requests.get(url) 
@@ -23,28 +19,37 @@ def parse():
     allAuthors = soup.findAll('div', class_='product-title__author') 
     allNames = soup.findAll('div', class_='product-title__head' )
     allPrices = soup.findAll('div', class_='product-price__value')
-    def sortExcel(): #Функция заполнения ячеек Excel
-        i = 1
-        j = 1
-        for auth in allAuthors:
-            if auth is not None:
-                ws.cell(row=j, column =i).value = auth.text        
-                j += 1
-        i += 1
-        j = 1
-        for name in allNames:
-            ws.cell(row=j, column = i).value = name.text
+    sortExcel(allAuthors, allNames, allPrices)
+
+def sortExcel(authors, names, prices): #Функция заполнения ячеек Excel
+
+    doc = load_workbook(filename='goo.xlsx')
+    ws = doc.active #Создание книги Excel и рабочего листа
+
+    i = 1
+    j = 1
+    for auth in authors:
+        n_auth = " ".join(auth.text.split())
+        if n_auth is not None:
+            ws.cell(row=j, column =i).value = n_auth
             j += 1
-        i += 1
-        j = 1
-        for price in allPrices:
-            ws.cell(row=j, column = i).value = price.text
-            j += 1 
+    i += 1
+    j = 1
+    for name in names:
+        n_name = " ".join(name.text.split())
+        if n_name is not None:
+            ws.cell(row=j, column = i).value = n_name
+            j += 1
+    i += 1
+    j = 1
+    for price in prices:
+        n_price = " ".join(price.text.split())
+        if n_price is not None:
+            ws.cell(row=j, column = i).value = n_price
+            j += 1
 
-        doc.save('goo.xlsx')
+    doc.save('goo.xlsx')
 
-    sortExcel()
-parse()
 
 
 
---
+++ start.py
@@ -0,0 +1,5 @@
+import mainfile
+
+if __name__ == '__main__':
+    mainfile.parse()
+
